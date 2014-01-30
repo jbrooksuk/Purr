90e7579438ac5ec65e610d3c8e32c5b4478e4ade
James Brooks <jbrooksuk@me.com>
2014-01-30 21:44:56 +0000

    Don't need to clean up on unload yet

--- src/Purr.c
+++ src/Purr.c
@@ -8,15 +8,10 @@ static void window_load(Window *window) {
   GRect bounds = layer_get_bounds(window_layer);
 }
 
-static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
-}
-
 static void init(void) {
   window = window_create();
   window_set_window_handlers(window, (WindowHandlers) {
     .load = window_load,
-    .unload = window_unload,
   });
   const bool animated = false;
   window_stack_push(window, animated);
