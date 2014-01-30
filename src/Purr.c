7b7d9279a7cace0b7e12b2fdfac2a2babf72cb10
James Brooks <jbrooksuk@me.com>
2014-01-30 21:55:47 +0000

    Purr is vibrating

--- src/Purr.c
+++ src/Purr.c
@@ -3,9 +3,21 @@
 static Window *window;
 static TextLayer *text_layer;
 
+static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
+  int minute = tick_time->tm_min;
+
+  const int minuteinterval = 5;
+  if (minute % minuteinterval == 0) {
+    vibes_double_pulse();
+  }
+}
+
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
+
+  // Subscribe to the tick timer event service;
+  tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
 }
 
 static void init(void) {
@@ -23,8 +35,6 @@ static void deinit(void) {
 
 int main(void) {
   init();
-
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
 
   app_event_loop();
   deinit();
