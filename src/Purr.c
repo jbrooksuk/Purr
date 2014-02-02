452c5256a7c9919856656055ccb8f582207bd816
James Brooks <jbrooksuk@me.com>
2014-02-02 16:17:38 +0000

    Added Purr text that will be intro

--- src/Purr.c
+++ src/Purr.c
@@ -19,7 +19,11 @@ int main(void) {
 
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_frame(window_layer);
-  // text_layer = text_layer_create(bounds);
+  text_layer = text_layer_create((GRect){ .origin = { 0, 50 }, .size = bounds.size });
+  text_layer_set_text(text_layer, "Purr");
+  text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(text_layer));
 
   timer = app_timer_register(PURR_TIMEOUT, purr, NULL);
 
