73c1c01d4608656a0f3ff60a79966a89a144e930
James Brooks <jbrooksuk@me.com>
2014-02-02 15:18:09 +0000

    Rewrite Purr

--- src/Purr.c
+++ src/Purr.c
@@ -1,43 +1,25 @@
 #include <pebble.h>
 
-#define PURR_INTERVAL 5
+#define PURR_INTERVAL 1
 
 static Window *window;
 static TextLayer *text_layer;
+static AppTimer *timer;
 
-static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
-  int minute = tick_time->tm_min;
-
-  if (minute % PURR_INTERVAL == 0) {
-    vibes_double_pulse();
-  }
-}
-
-static void window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
-
-  // Subscribe to the tick timer event service;
-  tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
-}
-
-static void init(void) {
-  window = window_create();
-  window_set_window_handlers(window, (WindowHandlers) {
-    .load = window_load,
-  });
-  window_set_fullscreen(window, true);
-  const bool animated = false;
-  window_stack_push(window, animated);
-}
-
-static void deinit(void) {
-  window_destroy(window);
+static void purr(void *data) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Purr...");
+  vibes_long_pulse();
 }
 
 int main(void) {
-  init();
+  window = window_create();
+  window_stack_push(window, true);
+
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_frame(window_layer);
+  // text_layer = text_layer_create(bounds);
+
+  timer = app_timer_register(300000, purr, NULL);
 
   app_event_loop();
-  deinit();
 }
